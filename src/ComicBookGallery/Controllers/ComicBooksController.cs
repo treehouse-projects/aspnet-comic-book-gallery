a4eece66fc86fcba6ba11fc5af7a26efa6d2b484
James Churchill <james.churchill@teamtreehouse.com>
2016-05-17 15:14:09 -0700

    Added Detail action method

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
