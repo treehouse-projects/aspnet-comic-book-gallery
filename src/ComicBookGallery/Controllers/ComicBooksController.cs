10e655adbf0ed45a0f6ae4c8047d64919d8c9484
James Churchill <james.churchill@teamtreehouse.com>
2016-05-17 18:41:30 -0700

    Added Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controller!");
+            return View();
         }
     }
 }
