29a4d035e355e853d4f972447450a24bd436469b
James Churchill <james.churchill@teamtreehouse.com>
2016-05-19 16:36:42 -0700

    Updated Comic Book Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,18 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives...<strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
             return View();
         }
     }
