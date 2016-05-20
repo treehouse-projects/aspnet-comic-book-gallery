d7aa81016fb46f6ee5291966318eb6ad89652226
James Churchill <james.churchill@teamtreehouse.com>
2016-05-20 12:01:09 -0700

    Updated comic book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -13,5 +13,23 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        // series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
